9834e716ba1ec213ad75be5fe1981ba0bbba7848
keede <wesaq11@gmail.com>
2024-02-26 11:01:51 +0900

    feat: User Role Define

--- src/main/kotlin/io/keede/travly/domains/user/entity/UserEntity.kt
+++ src/main/kotlin/io/keede/travly/domains/user/entity/UserEntity.kt
@@ -2,10 +2,7 @@ package io.keede.travly.domains.user.entity
 
 import io.keede.travly.config.entity.BaseEntity
 import io.keede.travly.domains.security.AuthenticationDetail
-import jakarta.persistence.AttributeOverride
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 /**
@@ -19,14 +16,22 @@ class UserEntity(
     @Column(name = "email", nullable = false, length = 50)
     private val email: String,
     @Column(name = "password", nullable = false)
-    private val password: String
+    private val password: String,
+    @Enumerated(EnumType.STRING)
+    private val role: UserRole
 ) : BaseEntity() {
+
+    constructor(
+        email: String,
+        password: String
+    ): this(email, password, UserRole.MEMBER)
+
     fun toAuthenticationDetail(): AuthenticationDetail {
         return AuthenticationDetail(
             this.email,
             this.password,
-            listOf("ROLE_MEMBER").stream()
-                .map { role -> SimpleGrantedAuthority(role) }
+            listOf(this.role.typeName).stream()
+                .map(::SimpleGrantedAuthority)
                 .toList()
         )
     }
---
+++ src/main/kotlin/io/keede/travly/domains/user/entity/UserRole.kt
@@ -0,0 +1,13 @@
+package io.keede.travly.domains.user.entity
+
+/**
+ * @author keede
+ * Created on 2024/02/26
+ */
+enum class UserRole(
+    val typeName: String,
+) {
+
+    MEMBER("ROLE_MEMBER"),
+
+}
